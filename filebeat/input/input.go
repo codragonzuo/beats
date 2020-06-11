8ad06fa2166c35587b9aff8a48637926efbd1582
codragonzuo <codragon@163.com>
2020-06-11 15:45:32 +0800

    修改input

--- filebeat/input/input.go
+++ filebeat/input/input.go
@@ -26,7 +26,8 @@ import (
 	"github.com/elastic/beats/v7/filebeat/input/file"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/logp"
-	"github.com/elastic/beats/v7/libbeat/monitoring"
+	//"github.com/elastic/beats/v7/libbeat/monitoring"
+        "github.com/codragonzuo/beats/libbeat/monitoring"
 )
 
 var (
